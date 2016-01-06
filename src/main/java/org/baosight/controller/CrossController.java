72b27e9e63741939c30983a6eb772d0ed2ccd666
zhushenhuanghun <478816112@qq.com>
2016-01-06 11:35:11 +0800

    2016.01.06

--- src/main/java/org/baosight/controller/CrossController.java
+++ src/main/java/org/baosight/controller/CrossController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 
 //http://localhost:8080/JavaWeb/crossController/showCross.do
@@ -30,14 +31,25 @@ public class CrossController {
 		this.crossService = crossService;
 	}
 
-	@RequestMapping("/showCross/{crossid}")
+	@RequestMapping("/tojson/{crossid}")
 	@ResponseBody
-	public List showCross(@PathVariable Integer crossid,HttpServletRequest request){
+	public List tojson(@PathVariable Integer crossid){
 		List<Cross> l = new ArrayList<Cross>();
 		Cross c = crossService.getCrossByID(crossid);
-		request.setAttribute("cross", c);
+	
 		l.add(c);
 		return l;
+	}
+	
+	
+	@RequestMapping("/showCross/{crossid}")
+	public ModelAndView showCross(@PathVariable Integer crossid,HttpServletRequest request){
+		
+		Cross c = crossService.getCrossByID(crossid);		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("cross", c);
+		mv.setViewName("showCross");
+		return mv;
 	}
 
 }
